08147f389fa87161cff4e7033e5958c5c1699c08
nisan <dfildest@email.com>
2025-06-24 15:50:48 +0300

    refactor fetchPosts function to use async/await for improved readability

--- keremnet/src/App.tsx
+++ keremnet/src/App.tsx
@@ -4,35 +4,37 @@ import { PostBase } from './types/Post';
 import './App.css';
 
 export const App: React.FC = () => {
-
   const [posts, setPosts] = React.useState<PostBase[]>([]);
   const [loading, setLoading] = React.useState<boolean>(true);
 
   useEffect(() => {
-    fetch('http://localhost:3001/posts')
-      .then(response => response.json())
-      .then(data => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/posts');
+        const data = await response.json();
         setPosts(data);
-        setLoading(false);
-      })
-      .catch(error => {
+      } catch (error) {
         console.error('Error fetching posts:', error);
+      } finally {
         setLoading(false);
-      });
+      }
+    };
+    fetchPosts();
   }, []);
 
   return (
-    <div className="App">
-      <h1 className="app-title">Posts</h1>
-      {loading ? (
-        <p>Loading posts...</p>
-      ) : (
-        posts.length === 0 ? (
-          <h1>No posts available.</h1>
-        ) :
-        posts.map((post) => <Post key={post.id} {...post} />)
-      )}
-    </div>
+      <div className="App">
+        <h1 className="app-title">Posts</h1>
+        {loading ? (
+          <p>Loading posts...</p>
+        ) : (
+          posts.length === 0 ? (
+            <h1>No posts available.</h1>
+          ) :
+          posts.map((post) => <Post key={post.id} {...post} />)
+        )}
+      </div>
   );
 };
+
 export default App;
